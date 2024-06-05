e098a123eb000954aeafc13ebed987597854fedb
taufiqpop99 <taufiqpop52@gmail.com>
2024-06-05 19:08:59 +0700

    tutorial laravel 11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman About</h1>
+    <h3>Owner : {{ $nama }}</h3>
+    <img src="img/Pop.png" alt="Pop" width="200">
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
